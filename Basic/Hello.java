e9358ab00ab119b4d9a248fe3579b8b401c312b4
tonaor18 <116328653+tonaor18@users.noreply.github.com>
2022-10-21 11:11:05 +0700

    8555

--- Basic/Hello.java
+++ Basic/Hello.java
@@ -1,6 +1,8 @@
 public class  Hello{
      public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("My neme is tonaor");
+        System.out.println(18);
      }
 }
     
